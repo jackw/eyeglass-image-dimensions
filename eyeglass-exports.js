9c70ecd2664ca5a05094e5838f5a8c3793e0efd7
jackw <jack.westbrook@gmail.com>
2015-07-16 18:01:06 +0100

    More messing around whilst I try to understand eyeglass
    Assets are particularly troublesome.

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -1,16 +1,25 @@
 "use strict";
 
+var fs = require("fs");
 var path = require("path");
-var imageDimensions = require("./ImageDimensions");
+var sizeOf = require('image-size');
 
 module.exports = function(eyeglass, sass) {
-  return {
-    sassDir: path.join(__dirname, "sass"),
-    functions: {
-      "eyeglassImageSize($assetPath)":
-      function(assetPath, done) {
-        done(sass.types.String('"image path is ' + assetPath.getValue() + '"'));
-      }
-    }
-  };
+	return {
+		sassDir: path.join(__dirname, "sass"),
+		functions: {
+			"eyeglass-image-width($assetPath)":
+			function(assetPath, done) {
+				done(sass.types.String('"image width is ' + assetPath.getValue() + '"'));
+			},
+			"eyeglass-image-height($assetPath)":
+			function(assetPath, done) {
+				done(sass.types.String('"image height is ' + assetPath.getValue() + '"'));
+			},
+			"eyeglass-image-dimensions($assetPath)":
+			function(assetPath, done) {
+				done(sass.types.String('"image dimensions are ' + assetPath.getValue() + '"'));
+			}
+		}
+	};
 };
