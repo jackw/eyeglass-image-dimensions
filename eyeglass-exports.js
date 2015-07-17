d0b2f1cac2fd4d8fa3a406d3412747111beed47a
jackw <jack.westbrook@gmail.com>
2015-07-17 15:53:49 +0100

    Getting image dimensions into sass
    JS functions now return image dimensions courtesy of image-size module.

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -5,20 +5,33 @@ var path = require("path");
 var sizeOf = require('image-size');
 
 module.exports = function(eyeglass, sass) {
+	var sassUtils = require("node-sass-utils")(sass);
+
+	function getDimensions(file) {
+		return sizeOf(file);
+	}
+
 	return {
 		sassDir: path.join(__dirname, "sass"),
 		functions: {
 			"eyeglass-image-width($assetPath)":
 			function(assetPath, done) {
-				done(sass.types.String('"image width is ' + assetPath.getValue() + '"'));
+				var dimensions = getDimensions(assetPath.getValue());
+				var width = new sassUtils.SassDimension(dimensions.width, "px");
+				done(sass.types.String(width.sassString()));
 			},
 			"eyeglass-image-height($assetPath)":
 			function(assetPath, done) {
-				done(sass.types.String('"image height is ' + assetPath.getValue() + '"'));
+				var dimensions = getDimensions(assetPath.getValue());
+				var height = new sassUtils.SassDimension(dimensions.height, "px");
+				done(sass.types.String(height.sassString()));
 			},
 			"eyeglass-image-dimensions($assetPath)":
 			function(assetPath, done) {
-				done(sass.types.String('"image dimensions are ' + assetPath.getValue() + '"'));
+				var dimensions = getDimensions(assetPath.getValue());
+				var width = new sassUtils.SassDimension(dimensions.width, "px");
+				var height = new sassUtils.SassDimension(dimensions.height, "px");
+				done(sass.types.String(width.sassString() + ' ' + height.sassString()));
 			}
 		}
 	};
